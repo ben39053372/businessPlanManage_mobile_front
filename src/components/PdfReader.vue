198c927dc4536074b47c0f231ae838360b0592d9
ben39053372 <ben39053372@gmail.com>
2019-08-20 10:23:16 +0800

    aasd

---
+++ src/components/PdfReader.vue
@@ -0,0 +1,15 @@
+<template>
+  <pdf ></pdf>
+</template>
+
+<script>
+import pdf from "vue-pdf";
+export default {
+  components:{
+    pdf
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
